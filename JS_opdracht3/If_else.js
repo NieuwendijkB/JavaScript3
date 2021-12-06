b1146de297b991af721369a787974d0b98e17204
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2021-12-06 22:21:51 +0100

    Add files via upload

---
+++ JS_opdracht3/If_else.js
@@ -0,0 +1,19 @@
+const age = 17;
+if (age < 18) {
+  console.log("Get outa here!");
+} else {
+  console.log("Welcome to the pub");
+}
+
+const isFemale = true;
+if (isFemale) {
+  console.log("drinks 50% off");
+} else {
+  console.log("full price for you buddy");
+}
+const driverStatus = "bob";
+if ((driverstatus = "bob")) {
+  console.log("Here are your keys legend");
+} else {
+  console.log("Start walking Drinky Mc Alcoholface.");
+}
